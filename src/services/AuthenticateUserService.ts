d660aa2a9a7d10275647440f641117c6aaa80194
mandimartins <mandimartins23@gmail.com>
2021-06-25 13:43:28 -0300

    aula 4

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,46 @@
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+import { compare } from "bcryptjs";
+
+import { sign } from "jsonwebtoken";
+
+interface IAuthenticateRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+  async execute({ email, password }: IAuthenticateRequest) {
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    const user = await usersRepositories.findOne({
+      email,
+    });
+
+    if (!user) {
+      throw new Error("Email/Password incorrect");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email/Password incorrect");
+    }
+
+    const token = sign(
+      {
+        email: user.email,
+      },
+      "66cc811a6dcbf1b64c69edacbf53e53d",
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+
+    return token;
+  }
+}
+
+export { AuthenticateUserService };
--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,19 +1,24 @@
 import { getCustomRepository } from "typeorm";
 import { UsersRepositories } from "../repositories/UsersRepositories";
 
+import { hash } from "bcryptjs";
+
 interface IUserRequest {
   name: string;
   email: string;
   admin?: boolean;
+  password: string;
 }
 
 class CreateUserService {
-  async execute({ name, email, admin }: IUserRequest) {
+  async execute({ name, email, admin, password }: IUserRequest) {
     const usersRepositories = getCustomRepository(UsersRepositories);
 
     if (!email) {
       throw new Error("Incorrect Email");
     }
+
+    const passwordHash = await hash(password, 8);
 
     const userAlreadyExists = await usersRepositories.findOne({
       email,
@@ -27,6 +32,7 @@ class CreateUserService {
       name,
       email,
       admin,
+      password: passwordHash,
     });
 
     await usersRepositories.save(user);
